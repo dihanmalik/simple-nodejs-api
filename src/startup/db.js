05473a93f78f6e23f253f343e6b6d26875b2502b
dihanmalik <abdulmaliknahid@gmail.com>
2020-06-08 02:48:52 +0800

    chore: put back mongodb connect option

--- src/startup/db.js
+++ src/startup/db.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 
 module.exports = function () {
   mongoose.set('useCreateIndex', true)
-  mongoose.connect(process.env.DB)
+  mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => winston.info('Connected to MONGODB...'))
 }
